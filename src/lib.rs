906d45fddb69b083042a824523cb9868960351bd
Prasanna Gautam <prasannagautam@gmail.com>
2024-06-04 19:46:39 -0700

    wasm stuff - not sure if it fully works yet

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,18 @@
 use bitcoin_script::define_pushable;
+use wasm_bindgen::prelude::*;
 
 pub mod database;
 
 pub mod spending_requirements;
 
 define_pushable!();
+
+#[wasm_bindgen]
+extern {
+    pub fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    alert(&format!("Hello, {}!", name));
+}
